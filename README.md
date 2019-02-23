73a3e5de59872606009875003040f89e3962d925
vic-lee <viclee@nyu.edu>
2019-02-23 17:48:34 -0500

    Rename main file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# os-scheduler
-OS Lab 2: Scheduler
+# Lab 2: Scheduling 
+
+1. FCFS: queue
+2. RR w/ quantum 2
+3. Uniprogrammed 
+4. Shortest Job First (SJF) 
---
+++ src/main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <queue> 
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  string fname = argv[0];
+  read_file(fname);
+	return 0;
+}
+
+string read_file(string fname) {
+  
+}
+
+void fifo_scheduler() {
+  
+}
