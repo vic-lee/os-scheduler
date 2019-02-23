03d1c7fabb2698af57a7f5908cd2a96c2a0e87ff
vic-lee <viclee@nyu.edu>
2019-02-23 18:10:00 -0500

    Add read file function

--- src/main.cc
+++ src/main.cc
@@ -4,16 +4,34 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-  string fname = argv[0];
-  read_file(fname);
-	return 0;
-}
 
 string read_file(string fname) {
-  
+	string out = "";
+
+	ifstream input_file(fname);
+	if (input_file.is_open()) {
+		string val;
+		while (input_file >> val) {
+			if (input_file.eof()) break;
+			std::cout << val;
+		}
+	} else {
+		std::cout << "File " << fname << " could not be opened." << std::endl;
+	}
+	input_file.close();
+
+	return out;
 }
 
+
 void fifo_scheduler() {
-  
+	
+}
+
+
+int main(int argc, char** argv) {
+	string fname = argv[1];
+	std::cout << fname << std::endl;
+	read_file(fname);
+	return 0;
 }
