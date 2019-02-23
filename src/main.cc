9c177624450d91bb0e5ac99cd17a16b5047b14de
vic-lee <viclee@nyu.edu>
2019-02-23 18:22:14 -0500

    Prevent reading comments in input file

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <queue> 
+#include <locale>
 
 using namespace std;
 
@@ -12,12 +13,13 @@ string read_file(string fname) {
 	if (input_file.is_open()) {
 		string val;
 		while (input_file >> val) {
-			if (input_file.eof()) break;
-			std::cout << val;
+			if (isalpha(val[0]) || input_file.eof()) break;
+			std::cout << val << " ";
 		}
 	} else {
 		std::cout << "File " << fname << " could not be opened." << std::endl;
 	}
+	std::cout << std::endl;
 	input_file.close();
 
 	return out;
@@ -31,7 +33,6 @@ void fifo_scheduler() {
 
 int main(int argc, char** argv) {
 	string fname = argv[1];
-	std::cout << fname << std::endl;
 	read_file(fname);
 	return 0;
 }
