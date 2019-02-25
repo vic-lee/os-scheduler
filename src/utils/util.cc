8e04b89eaf28e7143d858b3426bafc8590bf9834
vic-lee <viclee@nyu.edu>
2019-02-25 09:55:59 -0500

    Add default case in switch

--- src/utils/util.cc
+++ src/utils/util.cc
@@ -33,18 +33,11 @@ tuple<process*, int> read_file(string fname) {
             if (isalpha(val[0]) || input_file.eof()) break;
 
             switch (ctr) {
-                case 0: 
-                    temp_process.arrival_time = stoi(val);
-                    break;
-                case 1: 
-                    temp_process.init_interval = stoi(val);
-                    break;
-                case 2: 
-                    temp_process.cpu_time = stoi(val);
-                    break;
-                case 3: 
-                    temp_process.io_time = stoi(val);
-                    break;
+                case 0: temp_process.arrival_time = stoi(val); break;
+                case 1: temp_process.init_interval = stoi(val); break;
+                case 2: temp_process.cpu_time = stoi(val); break;
+                case 3: temp_process.io_time = stoi(val); break;
+                default: cout << "Error in parsing process." << endl; break;
             }
 
             if (ctr == EOPROCESS) {
