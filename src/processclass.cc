acd076059463ec89c063d6dd666774c2281da2cb
vic-lee <viclee@nyu.edu>
2019-02-28 18:57:47 -0500

    Update proc decr funcs

--- src/processclass.cc
+++ src/processclass.cc
@@ -4,16 +4,23 @@
 
 namespace scheduler {
 
-    void Process::decr_cpu_burst() {
+    void Process::decr_cpu_burst(RandNumAccessor rnum) {
         remaining_cpu_burst--;
         cpu_time--;
-        if (remaining_cpu_burst == 0) state = BLOCKED;
+        if (remaining_cpu_burst == 0) {
+            state = BLOCKED;
+            int burst = rnum.randomOS(interval);
+            interval = burst;
+            remaining_io_burst = burst;
+        }
     }
 
-    void Process::decr_io_burst() {
+    void Process::decr_io_burst(RandNumAccessor rnum) {
         remaining_io_burst--;
         io_time--;
-        if (remaining_io_burst == 0) state = READY;
+        if (remaining_io_burst == 0) {
+            state = READY;
+        }
     }
 
 }
