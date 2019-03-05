792200dc8fa2200907831d4ccdeb9e599b71cc6a
vic-lee <viclee@nyu.edu>
2019-03-04 23:54:30 -0500

    Create header file for process class

--- src/header.h
+++ src/header.h
@@ -26,43 +26,6 @@ namespace scheduler {
             std::tuple<int, int> randomOS(int u);
     };
 
-    class Process {
-        public: 
-            std::string state;
-            int pid;
-            int arrival_time;
-            int cpu_interval;
-            int cpu_time;
-            int io_interval;
-            int remaining_cpu_burst;
-            int assigned_cpu_burst;
-            int cpu_time_used;
-            int remaining_quantum = 0;
-            bool is_preempted = false;
-            int remaining_io_burst;
-            int terminated_time = 0;
-            int turnaround_time; 
-            int blocked_time = 0;   // time in the blocked state
-            int waiting_time = 0;   // time in the ready state
-            int cpu_time_assigned;
-            void set_state();
-            void ready_to_run(RandNumAccessor &rnum, int quantum = QT_UNDEF);
-            void running_to_blocked(RandNumAccessor &rnum);
-            void blocked_to_ready();
-            void run_to_ready();
-            void decr_cpu_burst();
-            void decr_io_burst();
-            bool is_finished();
-            bool is_cpu_burst_finished();
-            bool is_io_burst_finished();
-            void terminate_process(int cycle);
-            void calc_turnaround_time();
-            void incr_cpu_time_used();
-            void decr_remaining_quantum(int quantum);
-            void update_quantum_vars(int quantum);
-            int should_preempt(int quantum);
-    };
-
 
     /*      utils/calcproc.cc       */
     bool comp_proc(Process a, Process b);
